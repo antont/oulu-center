bbc02477245ef90e6b8dc0b85757d5b1fd0717f1
flciprian <flciprian.c@gmail.com>
2017-04-11 12:17:15 +0300

    randomize scale for trees

--- Assets/Trees.cs
+++ Assets/Trees.cs
@@ -8,10 +8,10 @@ public class Trees : MonoBehaviour
 
 	void Start ()
 	{
-//		print (group + " " + group.transform.childCount);
 		for (int i = 0; i < group.transform.childCount; i += 3) {
-//			print (i);
-			Instantiate (speedTree, group.transform.GetChild (i).position, Quaternion.Euler (0, Random.Range (0, 360), 0));
+			GameObject instance = Instantiate (speedTree, group.transform.GetChild (i).position, Quaternion.Euler (0, Random.Range (0, 360), 0));
+			float random = Random.Range (.5f, 1f);
+			instance.transform.localScale = new Vector3 (random, random, random); 
 			Destroy (group.transform.GetChild (i).gameObject);
 		}
 	}
