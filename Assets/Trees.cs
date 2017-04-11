fdd51e149c4d876e2c555a0ebb2bc6720412d765
flciprian <flciprian.c@gmail.com>
2017-04-11 12:31:25 +0300

    parent created trees to Trees game object

--- Assets/Trees.cs
+++ Assets/Trees.cs
@@ -9,9 +9,12 @@ public class Trees : MonoBehaviour
 	void Start ()
 	{
 		for (int i = 0; i < group.transform.childCount; i += 3) {
-			GameObject instance = Instantiate (speedTree, group.transform.GetChild (i).position, Quaternion.Euler (0, Random.Range (0, 360), 0));
+			GameObject instance = Instantiate (speedTree, 
+				                      group.transform.GetChild (i).position, 
+				                      Quaternion.Euler (0, Random.Range (0, 360), 0), 
+				                      transform);
 			float random = Random.Range (.5f, 1f);
-			instance.transform.localScale = new Vector3 (random, random, random); 
+			instance.transform.localScale = new Vector3 (random, random, random);
 			Destroy (group.transform.GetChild (i).gameObject);
 		}
 	}
