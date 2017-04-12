b6fba39acf61f2e768a7f8f05c3c047769c54f79
flciprian <flciprian.c@gmail.com>
2017-04-12 13:58:22 +0300

    Trees.cs changed to replace and deactivate all scene tree groups

--- Assets/Trees.cs
+++ Assets/Trees.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class Trees : MonoBehaviour
 {
-	public GameObject speedTree, group;
+	public GameObject speedTree;
+	public GameObject[] sceneTreeGroups;
 
 	void Start ()
 	{
-		for (int i = 0; i < group.transform.childCount; i += 3) {
-			GameObject instance = Instantiate (speedTree, 
-				                      group.transform.GetChild (i).position, 
-				                      Quaternion.Euler (0, Random.Range (0, 360), 0), 
-				                      transform);
-			float random = Random.Range (.5f, 1f);
-			instance.transform.localScale = new Vector3 (random, random, random);
-			Destroy (group.transform.GetChild (i).gameObject);
+		foreach (GameObject sceneTreeGroup in sceneTreeGroups) {
+			for (int i = 0; i < sceneTreeGroup.transform.childCount; i += 3) {
+				GameObject instance = Instantiate (speedTree, 
+					                      sceneTreeGroup.transform.GetChild (i).position, 
+					                      Quaternion.Euler (0, Random.Range (0, 360), 0), 
+					                      transform);
+				float random = Random.Range (.5f, 1f);
+				instance.transform.localScale = new Vector3 (random, random, random);
+			}
+			sceneTreeGroup.SetActive (false);
 		}
 	}
 }
